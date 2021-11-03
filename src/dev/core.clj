d0507f3dac7eaed7e6dbefa19c2d859157655a59
cpele <geekarist@users.noreply.github.com>
2021-11-03 19:36:49 +0100

    Copy normalize css resource

--- src/dev/core.clj
+++ src/dev/core.clj
@@ -1,7 +1,14 @@
-(ns dev.core)
+(ns dev.core
+  (:require [clojure.java.io :as io]))
 
-(defn hello-hook
+(defn build-normlz-css
   {:shadow.build/stage :flush}
   [build-state & _args]
-  (println "Hello")
+  (println (map #(str %)
+                (.list (io/file "resources" "public"))))
+  (io/copy (io/file "node_modules" "normalize.css" "normalize.css")
+           (io/file "resources" "public" "css" "normalize.css"))
   build-state)
+
+(comment
+  build-normlz-css)
