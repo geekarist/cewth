f96f9d73a2a59bb9ad60d8494cde624e362025b2
cpele <geekarist@users.noreply.github.com>
2021-11-04 08:17:37 +0100

    Move values to state

--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -4,7 +4,12 @@
 
 (enable-console-print!)
 
-(defonce state (atom 0))
+(defonce state (atom {:location "Bourron Marlotte, Île-de-France"
+                      :icon-src "http://localhost:3000/developer.accuweather.com/sites/default/files/13-s.png"
+                      :temperature-val 16
+                      :temperature-unit "℃"
+                      :weather-text "Mostly cloudy"
+                      :updated-at "Updated as of 15:40"}))
 
 (defn root-component []
   [:div.root-ctn
@@ -12,13 +17,13 @@
     [:input.search-txt {:type "text"}]
     [:button.search-btn {:on-click #(println "Yo")} "Search"]]
    [:div.result-ctn
-    [:div.location-ctn "Bourron Marlotte, Île-de-France"]
+    [:div.location-ctn (@state :location)]
     [:div.temperature-ctn
-     [:img.weather-icn {:src "http://localhost:3000/developer.accuweather.com/sites/default/files/13-s.png"}]
-     [:span.temperature-lbl "14"]
-     [:span.unit-lbl "℃"]]
-    [:div.weather-text-blk "Mostly cloudy"]
-    [:div.update-date-blk "Updated as of 15:40"]]])
+     [:img.weather-icn {:src (@state :icon-src)}]
+     [:span.temperature-lbl (@state :temperature-val)]
+     [:span.unit-lbl (@state :temperature-unit)]]
+    [:div.weather-text-blk (@state :weather-text)]
+    [:div.update-date-blk (@state :updated-at)]]])
 
 (defn ^:dev/after-load start! []
   (rd/render
