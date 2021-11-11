0ef97e3157b73300084ae4aa504a6faae7492c34
cpele <geekarist@users.noreply.github.com>
2021-11-11 10:18:34 +0100

    Call city search web service

--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -1,21 +1,76 @@
 (ns app.renderer.core
   (:require [reagent.core :refer [atom]]
-            [reagent.dom :as rd]))
+            [reagent.dom :as rd]
+            [ajax.core :as ajx]))
 
 (enable-console-print!)
 
-(defonce state (atom {:location "Bourron Marlotte, Île-de-France"
+(defonce state (atom {:query ""
+                      :location "Bourron Marlotte, Île-de-France"
                       :icon-src "http://localhost:3000/developer.accuweather.com/sites/default/files/13-s.png"
                       :temperature-val 16
                       :temperature-unit "℃"
                       :weather-text "Mostly cloudy"
                       :updated-at "Updated as of 15:40"}))
 
+(defn- update-state-query [state new-query]
+  (println new-query)
+  (assoc state :query new-query))
+
+(defn- update-state-result [state new-result]
+  (assoc state :location (new-result :name)))
+
+(defn- search! [query consume-query-result]
+  (letfn ^{:doc "Steps to search"}
+
+   [#_(1. Create city search request)
+    (to-city-search-req
+     [query]
+     {:uri "http://localhost:3000/dataservice.accuweather.com/locations/v1/cities/search"
+      :method :get
+      :params {:q query
+               :api-key "TODO"}
+      :format (ajx/json-request-format)
+      :response-format (ajx/json-response-format)})
+
+    #_(2. Send city search request)
+    (request-city-search
+     [req consume-resp]
+     (ajx/ajax-request (assoc req :handler consume-resp)))
+
+    #_(3. Convert city search response to query result)
+    (to-query-result
+     [city-search-resp]
+     {:name (-> city-search-resp
+                (second)
+                (first)
+                (get "LocalizedName"))})]
+
+    #_(Wire up steps)
+    (-> query
+        (to-city-search-req)
+        (request-city-search
+         (fn [city-search-resp]
+           (-> city-search-resp
+               (to-query-result)
+               (consume-query-result)))))))
+
+(comment
+  (search! "monti" #(println "Result: " %)))
+
 (defn root-component []
   [:div.root-ctn
    [:div.search-ctn
-    [:input.search-txt {:type "text"}]
-    [:button.search-btn {:on-click #(println "Yo")} "Search"]]
+    [:input.search-txt {:type "text"
+                        :on-change #(swap! state 
+                                           update-state-query 
+                                           (.-value (.-target %)))}]
+    [:button.search-btn
+     {:on-click 
+      (fn []
+        (search! (@state :query)
+                 #(swap! state update-state-result %)))}
+     "Search"]]
    [:div.result-ctn
     [:div.location-ctn (@state :location)]
     [:div.temperature-ctn
@@ -29,3 +84,6 @@
   (rd/render
    [root-component]
    (js/document.getElementById "app-container")))
+
+(comment
+  start!)
