04c75901a64e2a17ba626733d54f59bc8fcbfc68
cpele <geekarist@users.noreply.github.com>
2021-12-07 12:21:17 +0100

    Check if CSS outdated

--- src/dev/core.clj
+++ src/dev/core.clj
@@ -4,10 +4,14 @@
 (defn build-normlz-css
   {:shadow.build/stage :flush}
   [build-state & _args]
-  (println (map #(str %)
-                (.list (io/file "resources" "public"))))
-  (io/copy (io/file "node_modules" "normalize.css" "normalize.css")
-           (io/file "resources" "public" "css" "normalize.css"))
+  (let [input-file (io/file "node_modules" "normalize.css" "normalize.css")
+        output-file (io/file "resources" "public" "css" "normalize.css")
+        outdated? (not (= input-file output-file))]
+    (print "Normalize.css outdated?" outdated?)
+    (if outdated?
+      (do (println " do update CSS file")
+          (io/copy input-file output-file))
+      (println " don't update CSS file")))
   build-state)
 
 (comment
