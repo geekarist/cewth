8b120ed397c758aad8f7e95fd7cd0bfe4cf00a0e
cpele <geekarist@users.noreply.github.com>
2021-12-08 09:10:24 +0100

    Refactor to [init update view]

--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -5,14 +5,6 @@
 
 (enable-console-print!)
 
-(defn- update-state-query [state new-query]
-  (assoc state :query new-query))
-
-(defn- update-state-result [state new-result]
-  (assoc state
-         :location (new-result :name)
-         :failed (new-result :failed)))
-
 (defn- search! [query consume-query-result]
   (letfn #_(Define steps to search)
 
@@ -55,19 +47,32 @@
 
 (defonce state (atom init))
 
+(declare dispatch)
+
 (defn- change-query! [query]
-  (swap! state
-         update-state-query
-         query))
+  (compare-and-set! state
+                    @state
+                    (assoc state :query query)))
 
 (defn- execute-query! [trigger]
   (if (= trigger "Enter")
     (search! (@state :query)
-             #(swap! state update-state-result %))
+             #(dispatch [:take-search-result %]))
     nil))
 
-(defn update []
-  "todo")
+(defn- take-search-result! [result]
+  (compare-and-set!
+   state
+   @state
+   (assoc state
+          :location (result :name)
+          :failed (result :failed))))
+
+(defn update-fn [[action arg] state]
+  (condp = action
+    :change-query [state [:change-query arg]]
+    :execute-query [state [:execute-query arg]]
+    :take-search-result [(compare-and-set! state @state arg) nil]))
 
 (defn view [dispatch]
   [:div.root-ctn
@@ -91,10 +96,16 @@
     [:div.weather-text-blk (@state :weather-text)]
     [:div.update-date-blk (@state :updated-at)]]])
 
-(defn dispatch [[event arg :as _action]]
-  (condp = event
+(defn handle [[effect arg :as _action]]
+  (condp = effect
     :change-query (change-query! arg)
-    :execute-query (execute-query! arg)))
+    :execute-query (execute-query! arg)
+    :take-search-result (take-search-result! arg)))
+
+(defn dispatch [action]
+  (let [[new-state effect] (update-fn action @state)]
+    (handle effect)
+    (compare-and-set! state @state new-state)))
 
 (defn ^:dev/after-load start! []
   (rd/render
